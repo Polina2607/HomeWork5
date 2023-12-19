14c954a79521b06e85847be1e7f4fafd4c12f370
Polina <polya_vi_13@mail.ru>
2023-12-19 21:20:52 +0300

    Решение второй задачи

---
+++ 002/002.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int [,] array = new int [4,4];
+
+void CreateArray()     
+{
+    for (int i = 0; i < array.GetLength(0); i++)    
+    {   
+        for (int j = 0; j < array.GetLength(1); j++)   
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray()      
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] +" ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void ReplaceLines()
+{
+    for(int i=0; i<array.GetLength(1); i++)
+    {
+        int temp = array[0,i];
+        array[0,i] = array[array.GetLength(0)-1, i];
+        array[array.GetLength(0)-1, i] = temp;
+    }
+}
+CreateArray();
+PrintArray();
+ReplaceLines();
+PrintArray();
